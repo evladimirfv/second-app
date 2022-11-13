5ce793ef029926df7471ed473a1ee7d79c9709f9
Vladimir Flores <evladimirfv@gmail.com>
2022-11-13 05:35:02 -0500

    Simple list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import parse from 'html-react-parser';
+
+let categories = [
+  'English Rock',
+  'Spanish Rock',
+  'Salsa',
+  'Cumbia',
+  'Vallenato',
+  'Reggae',
+  'Reggeton',
+  'Folklorica'
+];
+
+
+let reducer = (accumulator, item) => {
+    				return accumulator + "<li>" + item + "</li>";
+                };
+
+let generateOL = '<ul>' + 
+				categories.reduce(reducer,"") 
+                + '</ul>';
+
+
+
+let content = 
+  (<div>
+    <h1> My top 10 favorite songs per category </h1>
+    <h2>Categories:</h2>
+    <div>{parse(generateOL)} </div>
+    
+
+  </div>);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( content
+   
   );
 }
 
